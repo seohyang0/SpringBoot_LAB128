fb7a690251dd5943ed337973b5354023237fb5e6
hyng7 <hyng7@Shji>
2023-02-17 14:59:13 +0900

    2023-02-17 파일구조변경

--- Lab128/src/main/java/com/itnwe/Lab128/TableSpaceApplication.java
+++ Lab128/src/main/java/com/itnwe/Lab128/TableSpaceApplication.java
@@ -7,12 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 스프링부트의 자동 설정, 스프링 Bean 읽기와 생성이 모두 자동으로 설정됨
  * 해당 어노테이션이 있는 위치부터 설정을 읽어가기 때문에 항상 최상단에 위치
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.itnwe.Lab128.mapper.TableSpaceMapper"})
 public class TableSpaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TableSpaceApplication.class, args);
-
 	}
 
 }
--- Lab128/src/main/java/com/itnwe/Lab128/service/TableSpaceService.java
+++ Lab128/src/main/java/com/itnwe/Lab128/service/TableSpaceService.java
@@ -1,16 +1,17 @@
-package service;
+package com.itnwe.Lab128.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import domain.TableSpaceDao;
-import mapper.TableSpaceMapper;
+import com.itnwe.Lab128.domain.TableSpaceDao;
+import com.itnwe.Lab128.mapper.TableSpaceMapper;
+
 
 @Service
 public class TableSpaceService {
 	@Autowired private TableSpaceMapper m;
-	public List<TableSpaceDao> getTableSpace(){
-        return m.getTableSpace();
-    }
+		public List<TableSpaceDao> getTableSpace() {
+			return m.getTableSpace();
+		}
 }
