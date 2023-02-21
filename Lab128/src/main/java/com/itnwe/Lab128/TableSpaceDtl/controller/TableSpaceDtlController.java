582b8ae62196495b6ea54c60a47b0a9e11ad2377
abcd <abcd@DESKTOP-DJ7J5VE>
2023-02-21 16:57:51 +0900

    Merge branch 'master' of https://github.com/seohyang0/SpringBoot_LAB128 into HEAD

--- Lab128/src/main/java/com/itnwe/Lab128/TableSpaceDtl/controller/TableSpaceDtlController.java
+++ Lab128/src/main/java/com/itnwe/Lab128/TableSpaceDtl/controller/TableSpaceDtlController.java
@@ -1,5 +1,12 @@
 package com.itnwe.Lab128.TableSpaceDtl.controller;
 
-public class TableSpaceDtlController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller //컨트롤러 선언
+public class TableSpaceDtlController {
+	@RequestMapping("/TableSpaceDtl")
+    public String index() {
+		return "TableSpaceDtl"; // tamplates/index.html
+    }
 }
